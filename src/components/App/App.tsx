50915e1e7efb4b3e1c6cfa830b9b93287d6a2e39
Oksanamsangi <162930811+Oksanamsangi@users.noreply.github.com>
2025-07-25 00:29:54 -0600

    fixed

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -11,62 +11,66 @@ import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import type { Movie } from "../../types/movie";
 import { fetchMovies } from "../../services/movieService";
 
-function App() {
+export default function App() {
   const [movies, setMovies] = useState<Movie[]>([]);
   const [query, setQuery] = useState("batman");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+
   useEffect(() => {
     if (!query) return;
+
     const load = async () => {
       try {
         setLoading(true);
         setError(false);
         const result = await fetchMovies(query);
+
         if (result.length === 0) {
-          toast("There was an error, please try again");
+          toast("No movies found for your request");
         }
+
         setMovies(result);
       } catch (err) {
-        console.log(err);
-        toast(`Failed to fetch ${query}`);
+        console.error(err);
+        toast.error(`Failed to fetch ${query}`);
         setError(true);
       } finally {
         setLoading(false);
       }
     };
+
     load();
   }, [query]);
-  
+
   const handleSelect = (movie: Movie) => {
     setSelectedMovie(movie);
   };
-  const handelClosedModal = () => {
+
+  const handleCloseModal = () => {
     setSelectedMovie(null);
   };
 
   return (
     <>
       <Toaster position="top-right" />
+
       <SearchBar
-        action={async (formData) => {
-          const query = (formData.get("query") as string).trim();
-          if (query) {
-            setQuery(query);
-          }
+        onSubmit={(query) => {
+          setQuery(query.trim());
         }}
       />
+
       {loading && <Loader />}
       {error && <ErrorMessage />}
       {!loading && !error && (
         <MovieGrid movies={movies} onSelect={handleSelect} />
       )}
+
       {selectedMovie && (
-        <MovieModal movie={selectedMovie} onClose={handelClosedModal} />
+        <MovieModal movie={selectedMovie} onClose={handleCloseModal} />
       )}
     </>
   );
 }
-
-export default App;
--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,39 +1,37 @@
 import styles from "./SearchBar.module.css";
 import { toast } from "react-hot-toast";
 
+
 interface SearchBarProps {
-  action: (formData: FormData) => void | Promise<void>;
+  onSubmit: (query: string) => void | Promise<void>;
 }
-export default function SearchBar({ action }: SearchBarProps) {
+
+export default function SearchBar({ onSubmit }: SearchBarProps) {
+  const handleSubmit = async (formData: FormData) => {
+    const query = (formData.get("query") as string)?.trim();
+
+    if (!query) {
+      toast.error("No movies found for your request");
+      return;
+    }
+
+    await onSubmit(query);
+  };
 
   return (
     <div>
       <header className={styles.header}>
-        {" "}
         <div className={styles.container}>
-          {" "}
           <a
             className={styles.link}
             href="https://www.themoviedb.org/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Powered by TMDB    {" "}
-          </a>{" "}
-          <form
-            className={styles.form}
-            action={async (formData: FormData) => {
-              const query = (formData.get("query") as string)?.trim();
+            Powered by TMDB
+          </a>
 
-              if (!query) {
-                toast.error("No movies found for your request");
-                return;
-              }
-
-              await action(formData);
-            }}
-          >
-            {" "}
+          <form className={styles.form} action={handleSubmit}>
             <input
               className={styles.input}
               type="text"
@@ -41,13 +39,14 @@ export default function SearchBar({ action }: SearchBarProps) {
               autoComplete="off"
               placeholder="Search movies..."
               autoFocus
-            />{" "}
+            />
             <button className={styles.button} type="submit">
-              Search      {" "}
-            </button>{" "}
-          </form>{" "}
+              Search
+            </button>
+          </form>
         </div>
       </header>
     </div>
   );
 }
+
